77267d65da5cb0919367e49dd6dc22211c16f4ce
Felipe Soares <felipeluizsoares@gmail.com>
2018-03-02 16:14:47 -0300

    Update devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
+  devise_for :users, path: '',
+  controllers: {
     registrations: 'registrations'
+  },
+  path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    sign_up: 'register',
+
   }
 
-  root 'landing#index'
+ root 'landing#index'
 end
