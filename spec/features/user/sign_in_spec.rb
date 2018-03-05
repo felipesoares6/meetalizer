ea00a20ab59efa38e32d31832cc62247a68c2762
Felipe Soares <felipeluizsoares@gmail.com>
2018-03-05 20:31:13 -0300

    Add feature test to Sign In

---
+++ spec/features/user/sign_in_spec.rb
@@ -0,0 +1,29 @@
+feature 'Sign in' do
+  given(:user) { create(:user) }
+
+  scenario 'Sign in with correct credentials' do
+    visit login_path
+
+    within '[data-test="sign_in_form"]' do
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+
+      click_button 'Log in'
+    end
+
+    expect(page).to have_content 'Signed in successfully'
+  end
+
+  scenario 'Sign in with invalid credentials' do
+    visit login_path
+
+    within '[data-test="sign_in_form"]' do
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: 'wrongpassword'
+
+      click_button 'Log in'
+    end
+
+    expect(page).to have_content 'Invalid Email or password'
+  end
+end
