eb5d52622ec206505647a187bb95f900121d9a27
Felipe Soares <felipeluizsoares@gmail.com>
2018-03-15 23:00:35 -0300

    Add feature test to create a group

---
+++ spec/features/group/user_should_be_able_to_create_a_group_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+feature 'User should be able to create a group' do
+  given (:group) { build(:group) }
+  given (:user) { create(:user) }
+
+  before do
+    login_as(user)
+  end
+
+  scenario 'Create a group when the fields are valid' do
+    visit new_group_path
+
+    within '[data-test="create_group_form"]' do
+      fill_in 'Name', with: group.name
+      fill_in 'Description', with: group.description
+      fill_in 'Region', with: group.region
+      fill_in 'Profile picture url', with: group.profile_picture_url
+      fill_in 'Cover picture url', with: group.cover_picture_url
+
+      click_button 'Save Group'
+    end
+
+    expect(page).to have_current_path(groups_path)
+  end
+
+  scenario 'Do not create a group when the fields are valid' do
+    visit new_group_path
+
+    within '[data-test="create_group_form"]' do
+      fill_in 'Name', with: group.name
+      fill_in 'Description', with: group.description
+      click_button 'Save Group'
+    end
+
+    expect(page).to have_current_path(groups_path)
+    expect(page).to have_content 'Region can\'t be blank'
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'support/factory_bot'
+include Warden::Test::Helpers
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
