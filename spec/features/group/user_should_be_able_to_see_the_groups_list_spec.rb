9bd9aaca7a3fde98f4f91475d5a3ab891257ebee
Felipe Soares <felipeluizsoares@gmail.com>
2018-03-16 01:12:24 -0300

    Add feature test to list groups

---
+++ spec/features/group/user_should_be_able_to_see_the_groups_list_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'User should be able to see the groups list' do
+  scenario 'List the groups list when groups were created' do
+    create(:group)
+    visit groups_path
+
+    within '[data-test="groups_list"]' do
+      expect(page).to have_content('Name: name')
+      expect(page).to have_content('Description: description')
+      expect(page).to have_content('Region: region')
+    end
+  end
+
+  scenario 'Show no groups message when no groups were created' do
+    visit groups_path
+
+    within '[data-test="groups_list"]' do
+      expect(page).to have_content('No meetups were created yet, maybe you can create the first?')
+    end
+  end
+end
