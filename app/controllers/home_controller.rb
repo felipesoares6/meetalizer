76f01bd87d7ac5da7687051853a98e7a03031a97
Felipe Soares <felipeluizsoares@gmail.com>
2018-04-08 00:59:58 -0300

    Add user admin groups to home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     @user = current_user
+
+    @groups = Group.includes(:memberships).where(memberships: { user_id: @user.id, role: :admin })
   end
 end
