ac8556e618f440323b907658e81915af4e7f428d
Felipe Soares <felipeluizsoares@gmail.com>
2018-04-07 23:52:31 -0300

    Add home as root when user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   include Pundit
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -9,5 +8,9 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
+  end
+
+  def sign_in_and_redirect(resource)
+    home_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users,
-  controllers: {
-    registrations: 'registrations'
-  },
-  path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    sign_up: 'register',
-  }
+  devise_for :users, controllers: { registrations: 'registrations' },
+  path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
   as :user do
     get 'login', to: 'devise/sessions#new'
   end
 
+  authenticate :user do
+    root 'home#index'
+  end
+
+  root 'landing#index'
+
   resources :groups, only: [:index, :new, :create, :edit, :update, :show]
- root 'landing#index'
 end
