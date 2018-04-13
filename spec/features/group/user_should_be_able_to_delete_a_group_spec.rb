685d8a1943513b80f328995de9e1e3bcd5f93d45
Felipe Soares <felipeluizsoares@gmail.com>
2018-04-12 23:11:56 -0300

    Add tests to delete a group

--- spec/features/group/user_should_be_able_to_delete_a_group_spec.rb
+++ spec/features/group/user_should_be_able_to_delete_a_group_spec.rb
@@ -1,31 +1,21 @@
 require 'rails_helper'
 
-feature 'User should be able to update a group' do
+feature 'User should be able to delete a group' do
   context 'when the user has the admin role' do
     given (:group) { create(:group) }
     given (:user) { create(:user) }
     given! (:membership) { create(:membership, user_id: user.id, group_id: group.id, role: :admin) }
 
-    scenario 'Update a group successfully' do
+    scenario 'Delete a group successfully' do
       login_as(user)
 
       visit root_path
 
-      click_link group.name
+      click_link 'name'
 
-      click_link 'Edit'
+      click_button 'Delete'
 
-      within '[data-test="update_group_form"]' do
-        fill_in 'Name', with: group.name
-        fill_in 'Description', with: group.description
-        fill_in 'Region', with: group.region
-        fill_in 'Profile picture url', with: group.profile_picture_url
-        fill_in 'Cover picture url', with: group.cover_picture_url
-
-        click_button 'Save Group'
-      end
-
-      expect(page).to have_current_path(group_path(group.id))
+      expect(page).to have_current_path(root_path)
     end
   end
 
@@ -34,7 +24,7 @@ feature 'User should be able to update a group' do
     given (:user) { create(:user) }
     given! (:membership) { create(:membership, user_id: user.id, group_id: group.id, role: :member) }
 
-    scenario 'Do not update a group' do
+    scenario 'Do not delete a group' do
       login_as(user)
 
       visit root_path
@@ -44,7 +34,7 @@ feature 'User should be able to update a group' do
       click_link 'name'
 
       expect(page).to have_current_path(group_path(group.id))
-      expect(page).not_to have_content('Edit')
+      expect(page).not_to have_content('Delete')
     end
   end
 end
