eee978febf1d181f0615586bf596e60533d7b960
Felipe Soares <felipeluizsoares@gmail.com>
2018-04-14 13:57:32 -0300

    Add new model tests

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -5,4 +5,6 @@ RSpec.describe Group, type: :model do
   it { is_expected.to validate_presence_of :description }
   it { is_expected.to validate_presence_of :region }
   it { is_expected.to validate_length_of(:description).is_at_most(130) }
+  it { is_expected.to have_many :memberships }
+  it { is_expected.to have_many :users }
 end
