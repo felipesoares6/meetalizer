e6da77cff1160182334bba7ff697b1de0e74ed63
Felipe Soares <felipeluizsoares@gmail.com>
2018-04-19 15:04:08 -0300

    Update dashboard controller tests

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,18 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController do
-  let (:group) { create(:group) }
+  let (:group_as_admin) { create(:group) }
+  let (:group_as_member) { create(:group) }
+
   let (:user) { create(:user) }
-  let! (:membership) { create(:membership, user_id: user.id, group_id: group.id, role: :admin) }
+
+  let! (:membership_as_admin) { create(:membership, user_id: user.id, group_id: group_as_admin.id, role: :admin) }
+  let! (:membership_as_member) { create(:membership, user_id: user.id, group_id: group_as_member.id, role: :member) }
 
   describe 'GET index' do
     before do
       sign_in(user)
+
       get :index
     end
 
-    it 'assigns @groups' do
-      expect(assigns(:groups)).to eq([group])
+    it 'assigns @groups_as_admin' do
+      expect(assigns(:groups_as_admin)).to match_array([group_as_admin])
     end
 
     it 'renders the index template' do
