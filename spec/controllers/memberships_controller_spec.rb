93992db3f361bb13d32ab831443a60330925896b
Felipe Soares <felipeluizsoares@gmail.com>
2018-04-20 22:29:04 -0300

    Update memberships controller tests with destroy method

--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -16,7 +16,32 @@ RSpec.describe MembershipsController do
     it 'redirect to root' do
       post :create, params: { group_id: group.id }
 
-      expect(response).to redirect_to(root_path)
+      expect(response).to redirect_to(group_path(group.id))
+    end
+  end
+
+  describe 'DELETE destroy' do
+    let!(:membership) {
+      create(
+        :membership,
+        user_id: user.id,
+        group_id: group.id,
+        role: :normal
+      )
+    }
+
+    before { sign_in(user) }
+
+    it 'destroy a membership' do
+      expect {
+        delete :destroy, params: { group_id: group.id }
+      }.to change { Membership.count }.by(-1)
+    end
+
+    it 'redirect to group path' do
+      delete :destroy, params: { group_id: group.id }
+
+      expect(response).to redirect_to(group_path(group.id))
     end
   end
 end
