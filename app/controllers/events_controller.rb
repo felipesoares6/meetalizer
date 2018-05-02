e2af36bf46a795b61f04ba7282a0e16539154adf
Felipe Soares <felipeluizsoares@gmail.com>
2018-05-02 01:10:17 -0300

    Add create events action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,33 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group, only: [:index, :new, :create]
+
   def index
-    @events = Group.find(params[:group_id]).events
+    @events = @group.events
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.group = @group
+
+    if @event.save
+      redirect_to root_path
+    else
+      flash[:errors] = @event.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+  def find_group
+    @group = Group.find_by(id: params[:group_id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :description, :address, :start_date, :end_date, :cover_picture_url)
   end
 end
