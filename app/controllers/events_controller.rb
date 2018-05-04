69d687e5bcc92a86372efba08a41b34209bfba94
Felipe Soares <felipeluizsoares@gmail.com>
2018-05-04 10:57:15 -0300

    Add update event action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,13 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group, only: [:index, :new, :create]
+  before_action :find_group, only: [:index, :show, :new, :create, :edit, :update]
+  before_action :find_event, only: [:show, :edit, :update]
 
   def index
     @events = @group.events
+  end
+
+  def show
   end
 
   def new
@@ -15,16 +19,32 @@ class EventsController < ApplicationController
     @event.group = @group
 
     if @event.save
-      redirect_to root_path
+      render :show
     else
       flash[:errors] = @event.errors.full_messages
       render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to group_event_path(@group, @event)
+    else
+      flash[:errors] = @event.errors.full_messages
+      render :edit
+    end
+  end
+
   private
   def find_group
-    @group = Group.find_by(id: params[:group_id])
+    @group = Group.find_by!(id: params[:group_id])
+  end
+
+  def find_event
+    @event = @group.events.find_by!(id: params[:id])
   end
 
   def event_params
