cac83b76e750d46ab65eb6105b87c65506a4c6d5
Felipe Soares <felipeluizsoares@gmail.com>
2018-06-02 03:43:34 -0300

    Add tests to full? method on event model

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     address 'address'
     start_date Date.current
     end_date Date.current
+    rsvps_limit 50
     group
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -10,4 +10,55 @@ RSpec.describe Event, type: :model do
   it { is_expected.to have_many :organizers }
   it { is_expected.to have_many :event_rsvps }
   it { is_expected.to have_many :rsvps }
+
+  def full?
+    rsvps_limit <= event_rsvps.where(answer: true).count
+  end
+
+  describe '#full?' do
+    let(:group) { create(:group) }
+    let(:attendee) { create(:user) }
+    let(:user) { create(:user) }
+    let(:membership) do
+      create(:membership, user_id: user.id, group_id: group.id, role: :admin)
+    end
+
+    context 'and the event is full' do
+      let(:event) { create(:event, group_id: group.id, rsvps_limit: 1) }
+      let(:event_organizer) do
+        create(:event_organizer, user_id: user.id, event_id: event.id)
+      end
+      let!(:event_rsvp) do
+        create(
+          :event_rsvp,
+          user_id: attendee.id,
+          event_id: event.id,
+          answer: true
+        )
+      end
+
+      it 'return true' do
+        expect(event.full?).to equal(true)
+      end
+    end
+
+    context 'and the event is not full' do
+      let(:event) { create(:event, group_id: group.id, rsvps_limit: 2) }
+      let(:event_organizer) do
+        create(:event_organizer, user_id: user.id, event_id: event.id)
+      end
+      let!(:event_rsvp) do
+        create(
+          :event_rsvp,
+          user_id: attendee.id,
+          event_id: event.id,
+          answer: true
+        )
+      end
+
+      it 'return false' do
+        expect(event.full?).to equal(false)
+      end
+    end
+  end
 end
