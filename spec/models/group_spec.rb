662950fb471febf1bac926e8f8125ab2991ff027
Felipe Soares <felipeluizsoares@gmail.com>
2018-06-08 01:45:56 -0300

    Update group  model tests

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -8,4 +8,70 @@ RSpec.describe Group, type: :model do
   it { is_expected.to have_many :memberships }
   it { is_expected.to have_many :users }
   it { is_expected.to have_many :events }
+
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  describe '#admin?' do
+    context 'when the user is an admin' do
+      let!(:membership) do
+        create(
+          :membership,
+          user_id: user.id,
+          group_id: group.id,
+          role: :admin
+        )
+      end
+
+      it 'return true' do
+        expect(group.admin?(user)).to equal(true)
+      end
+    end
+
+    context 'when the user is not an admin' do
+      let!(:membership) do
+        create(
+          :membership,
+          user_id: user.id,
+          group_id: group.id,
+          role: :normal
+        )
+      end
+
+      it 'return false' do
+        expect(group.admin?(user)).to equal(false)
+      end
+    end
+  end
+
+  describe '#member?' do
+    context 'when the user is a member' do
+      let!(:membership) do
+        create(
+          :membership,
+          user_id: user.id,
+          group_id: group.id,
+          role: :normal
+        )
+      end
+      it 'return true' do
+        expect(group.member?(user)).to equal(true)
+      end
+    end
+
+    context 'when the user is not a member' do
+      let!(:membership) do
+        create(
+          :membership,
+          user_id: user.id,
+          group_id: group.id,
+          role: :admin
+        )
+      end
+
+      it 'return false' do
+        expect(group.member?(user)).to equal(false)
+      end
+    end
+  end
 end
